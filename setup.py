df0950abf4b4925e557c90f5b14ec4bdfa717d6d
guyskk <guyskk@qq.com>
2018-07-02 22:28:10 +0800

    bump version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='newio-rethinkdb',
-    version='0.1.0',
+    version='0.1.1',
     keywords='newio rethinkdb async driver',
     description='Newio + RethinkDB: Async RethinkDB driver',
     long_description=__doc__,
